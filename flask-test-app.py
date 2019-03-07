56ccd2d4e6f3b02e99e6a53cfc0de6981e85d531
root <root@lyubomir.info>
2019-03-08 00:17:50 +0200

    new file

---
+++ flask-test-app.py
@@ -0,0 +1,25 @@
+from flask import Flask
+import requests
+from time import sleep
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, World!"
+
+
+
+@app.route("/bomba")
+def get_data():
+    count = 1
+    url = 'https://lyubo.info'
+    resp = requests.get(url).content
+    while (count < 5):
+            print(resp)
+            count +=1
+            sleep(1)
+    a = f"'{url}' was called {count} times"   
+    return a
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=555, debug=True)
